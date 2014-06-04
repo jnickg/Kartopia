525592a7bcfe7473064309addfda56aadf138d78
Levi Schoen <ls3@pdx.edu>
2014-06-03 18:37:57 -0700

    Update OrderServices.cs

--- OrderManager/OrderServices.cs
+++ OrderManager/OrderServices.cs
@@ -17,12 +17,7 @@ public class TestClass
 		Console.WriteLine ("Order at Kartopia!\n");
 
 		MenuItem testMenuItem = new MenuItem (15.34, "hamburger");
-
-		testMenuItem.displayMenuItem();
-
-		Console.WriteLine ("Item cost : {0}", testMenuItem.getCost());
-		Console.WriteLine ();
-	}
+			}
 }
 ////Class for managing a menu item object
 public	class MenuItem
@@ -31,71 +26,43 @@ public	class MenuItem
 		public MenuItem()
 		{
 ///Set data members to zero vales
-			cost = 0;
-			name = null;
+		Cost = 0;
+		Name = null;
 		}
 ///MenuItem class constructor with args for data membrs
 		public MenuItem(double itemCost, String itemName)
 		{
 ///Set data members to the values passed in
-			name = itemName;
-			cost = itemCost;
+		Name = itemName;
+		Cost = itemCost;
 		}
 	///MenuItem class copy constructor
 		public MenuItem(MenuItem toCopy)
 		{
 		///Intialize data members to the same as the MenuItem object passed in
-			cost = toCopy.cost;
-		name = toCopy.name;
+		Cost = toCopy.Cost;
+		Name = toCopy.Name;
 		}
-	///MenuItem public function to return the cost of this menu item
-		public double  getCost()
-		{
-		///Return the cost of this menu item
-			return cost;
-		}
-	///MenuItem public function to return the name of this menu item
-		public String getName()
-		{
-		///Return the name of this menu item
-			return name;
-		}
+
 	///MenuItem public function to display the contents of the object
-	public int displayMenuItem()
+	public int displayMenuItem(TextWriter writer)
 	{
 		///Display the name and cost of the menu item
-		Console.WriteLine("Item : {0}", name);
-		Console.WriteLine("Cost : {0}", cost);
-		///Return success flag
-		return 1;
-	}
-	///MenuItem class public function for setting the data members of a MenuItem
-	///directly from the user
-	public int readMenuItemInfo()
-	{
-		///Prompt the user for MenuItem info, read it, and set the data members accordingly
-		Console.WriteLine ("\nEnter the name of Menu Item : ");
-		name = Console.ReadLine ();
-
-		Console.WriteLine ("\nEnter the cost of the menu item in $$.cents format : ");
-		cost = Convert.ToDouble(Console.ReadLine ());
-		///Return success flag
-		return 1;
-	}
-	///MenuItem class public function for setting the data members of a MenuItem object
-	///via paramaters
-	public int readMenuItemInfo(double itemCost, String itemName)
-	{
-		///Set data members to the values passed in
-		cost = itemCost;
-		name = itemName;
+		writer.WriteLine("Item : {0}", Name);
+		writer.WriteLine("Cost : {0}", Cost);
 		///Return success flag
 		return 1;
 	}
 	///MenuItem private data member to store the cost of the menu item
-		private double cost;
+	public double Cost {
+		get;
+		set;
+	}
 	///MenuItem private data member to store the name of the menu item
-		private String name;
+	public String Name {
+		get;
+		set;
+	}
 
 	}
 ///Class to manage an Order object, storing and manupilating order details
@@ -106,12 +73,12 @@ public	class Order
 		{
 		///Intialize data members to default values
 			items= null;
-		orderCost = 0;
-		orderStatus = 'c';
+		OrderCost = 0;
+		OrderStatus = 'c';
 		orderCounter = 0;
-		orderId = orderCounter++;
-		orderPickUpTime = (DateTime.Now).AddMinutes (40);
-		foodCartId = 0;
+		OrderId = orderCounter++;
+		OrderPickUpTime = (DateTime.Now).AddMinutes (40);
+		FoodCartId = 0;
 		}
 	///Order class constructor with arguements for a list of menu items
 	///and the id of the food cart making this order
@@ -119,18 +86,18 @@ public	class Order
 		{
 		///Intialize the items to the same items as passed in
 		items = new LinkedList<MenuItem> (newItems);
-		foodCartId = foodCartMaker;
+		FoodCartId = foodCartMaker;
 		///Intialize data members to appropriate values
-		orderCost = 0;
-		orderStatus = 's';
+		OrderCost = 0;
+		OrderStatus = 's';
 		orderCounter = 0;
-		orderId = orderCounter++;
-		orderPickUpTime = (DateTime.Now).AddMinutes (40);
+		OrderId = orderCounter++;
+		OrderPickUpTime = (DateTime.Now).AddMinutes (40);
 		///Node to help us in traversing the list of menu items
 		LinkedListNode<MenuItem> head = newItems.First;
 		///Loop to add the cost of each menu item to the cost of the order
 		while (head != null) {
-			orderCost += head.Value.getCost ();
+			OrderCost += head.Value.Cost;
 			head = head.Next;
 		}
 		}
@@ -139,173 +106,48 @@ public	class Order
 		{
 		///Set data members to the value of the Order object passed in
 		    items = new LinkedList<MenuItem> (toCopy.items);
-		orderPickUpTime = toCopy.orderPickUpTime;
-		orderCost = toCopy.orderCost;
-		orderStatus = toCopy.orderStatus;
-		orderId = toCopy.orderId;
-		foodCartId = toCopy.foodCartId;
+		OrderPickUpTime = toCopy.OrderPickUpTime;
+		OrderCost = toCopy.OrderCost;
+		OrderStatus = toCopy.OrderStatus;
+		OrderId = toCopy.OrderId;
+		FoodCartId = toCopy.FoodCartId;
 		}
-	///Order class public function to return the order status of this order object
-		public char getStatus()
-		{
-		///Return the order status
-			return orderStatus;
-		}
-	///Order class public function to set the order status of this order object
-	///There is no precondition that the status to be updated will be meaningfull
-	public char setStatus(char updatedStatus)
-		{
-		///Check to see if the udpated status is a delay status
-		if (updatedStatus == 'd')
-		{
-			///If it is delay the order pickup time by 5 minutes
-			orderPickUpTime.AddMinutes (5);
-			///Exit and leave the order's status intact
-			return this.getStatus ();
-		}
-		///Set the order status to the arguement passed in
-			orderStatus = updatedStatus;
-		///Return the updated order status
-			return this.getStatus ();
-	    }
-	///Order class public function to get the pickup time for this oder object
-	public DateTime getOrderPickUpTime()
-		{
-		///Return the pickup time for this order object
-			return orderPickUpTime;
-		}
-	///Order class public function to get the food cart id for this order object
-	public int getfoodCartId()
-	{
-		///Return the food cart id for this order object
-		return foodCartId;
-	}
-	///Order class public function to get the orderId associated with this order object
-	public int getOrderId()
-	{
-		///Return teh order id for this order object
-		return orderId;
-	}
 	///Order class public mention function to display the contents of the order objects
-	public int displayOrder()
+	public int displayOrder(TextWriter writer)
 	{
-		Console.WriteLine ();
+		writer.WriteLine ();
 		///Display order number
-		Console.WriteLine ("Order ID : {0}" , orderId);
+		writer.WriteLine ("Order ID : {0}" , OrderId);
 		///Display the id of the food cart making this order
-		Console.WriteLine ("Food Cart ID : {0}" ,foodCartId);
+		writer.WriteLine ("Food Cart ID : {0}" , FoodCartId);
 		///Display the
 		///Display all menu items this order contains
 		LinkedListNode<MenuItem> head = items.First;
 
-		Console.WriteLine ();
-		Console.WriteLine ("Menu Items : ");
+		writer.WriteLine ();
+		writer.WriteLine ("Menu Items : ");
 		///Loop to display all menu items contained in this order object
 		while (head != null)
 		{
-			head.Value.displayMenuItem ();
-			Console.WriteLine ();
+			head.Value.displayMenuItem (writer);
+			writer.WriteLine ();
 			head = head.Next;
 		}
 		///Display the order pickup time
-					Console.WriteLine ("Order Pick Up Time : {0}", orderPickUpTime.ToString());
-
+		writer.WriteLine ("Order Pick Up Time : {0}", OrderPickUpTime.ToString());
 		///Return success flag
 		return 1;
-
-	}
-	///Order class public function to create an order by taking
-	///input from a user
-	public int readOrderInfo()
-	{
-		///Integer to count loop iterations
-		int count = 0;
-		///character for storing users response
-		char response='Q';
-		///MenuItem object for storing a menu item
-		MenuItem tempItem = new MenuItem ();
-		///Do while loop to allow the user to add all the menu items they want
-		do {
-			///Prompt user for their choice
-			Console.WriteLine("\nEnter 'A' to add an item to this order");
-			Console.WriteLine("\nEnter 'I' to add the food cart Id for this order");
-			Console.WriteLine("\nEnter 'Q' to exit and submit finished order");
-			///Read user choice
-			response=Char.ToUpper(Convert.ToChar(Console.ReadLine()));
-
-			///If user wanted to add menu item
-			if(response=='A')
-			{
-				///If this is the first item to be added
-				if(count==0)
-				{
-					///Intialize the order's list of menu items
-					items=new LinkedList<MenuItem>();
-				}
-				///Read and store the new menu item for this order by calling
-				///the appropriate version of readMenuItemInfo function
-				tempItem.readMenuItemInfo();
-				///Add this menu to the end of the list of menu items for this order
-				items.AddLast(new LinkedListNode<MenuItem>(tempItem));
-				///Add the cost of this menu item to order cost
-				orderCost+=tempItem.getCost();
-				}
-			else if(response=='I')
-			{
-				///Prompt the user for the food cart id
-				Console.WriteLine("\nEnter the ID number of the food cart for this order : ");
-				///Read and store user's response
-				foodCartId=Convert.ToInt32(Console.ReadLine());
-			}
-			///Else user wants to quit
-			else{
-				///Display exit message
-				Console.WriteLine("\nSubmitting Order");
-			}
-			///update the loop counter
-			++count;
-		} while(response == 'A');
-
-		///Set order data members to appropriate values
-		if (items != null) {
-			orderStatus = 's';
-			orderPickUpTime = (DateTime.Now).AddMinutes (40);
-		} else {
-			orderStatus = 'c';
-			orderPickUpTime = DateTime.Now;
-			///Return fail flag as no items have been added to this order
-			return 0;
 		}
-		///Return success flag
-		return 1;
-	}
-	///Order class public function to create an order through
-	///parameters passed into the function
-	public int readOrderInfo(LinkedList<MenuItem> newItems, int foodCartMaker)
-	{
-		///Intialize the items to the same items as passed in
-		items = new LinkedList<MenuItem> (newItems);
-		foodCartId = foodCartMaker;
-		///Intialize data members to appropriate values
-		orderCost = 0;
-		orderStatus = 's';
-		orderPickUpTime = (DateTime.Now).AddMinutes (40);
-		///Node to help us in traversing the list of menu items
-		LinkedListNode<MenuItem> head = newItems.First;
-		///Loop to add the cost of each menu item to the cost of the order
-		while (head != null) {
-			orderCost += head.Value.getCost ();
-			head = head.Next;
-		}
-		///Return success flag
-		return 1;
-	}
+
 	///Ordeer class private data member to aid in generating unique order id's
 	private static int orderCounter;
 	///Order class private datat member containing a linked list of menu items
 	private LinkedList<MenuItem> items;
 	///Order class private data member to track total cost of order
-	private double orderCost;
+	public double OrderCost {
+		get;
+		set;
+	}
 	///Order class private data member to track the status of the order
 	///Possible values include
 	///s-submitted
@@ -313,12 +155,35 @@ public	class Order
 	///p-prepping
 	///f-finished
 	///r-recieved
-		private char orderStatus;
-		private int orderId;
+	public char OrderStatus {
+		get{
+			return OrderStatus;
+		}
+		set{
+			///Check to see if the udpated status is a delay status
+			if (value == 'd')
+			{
+				///If it is delay the order pickup time by 5 minutes
+				OrderPickUpTime.AddMinutes (5);
+			}
+			///Set the order status to the arguement passed in
+			OrderStatus = value;
+		}
+	}
+	public int OrderId {
+		get;
+		set;
+	}
 	///Order class private data member for the food cart that is making this order object
-		private int foodCartId;
+	public int FoodCartId {
+		get;
+		set;
+	}
 	///Order class private data member to hold the pickup time for this order object
-	private DateTime orderPickUpTime;
+	public DateTime OrderPickUpTime {
+		get;
+		set;
+	}
 
 	}
 ///Class to manage a collection of orders
@@ -377,22 +242,22 @@ public	class OrderManager
 		}
 		}
 	///OrderManager class public function to view an order using the order id for a key
-	public int viewOrder(Order order)
+	public int viewOrder(Order order, TextWriter writer)
 		{
 		///Call the order objects display function
-		order.displayOrder ();
+		order.displayOrder (writer);
 		///Return success flag
 		return 1;
 		}
 	///OrderManager class public function to view a collection of orders by order id
-	public int viewOrders(LinkedList<Order> requestedOrders)
+	public int viewOrders(LinkedList<Order> requestedOrders, TextWriter writer)
 	{
 		///Node to help us in navigating the list of orders
 		LinkedListNode<Order> head = requestedOrders.First;
 		///Travese the linked list and display each order
 		while (head != null) {
 			///Display the order in the current node
-			head.Value.displayOrder();
+			head.Value.displayOrder(writer);
 			///Advance our node to the next object in the linked list
 			head = head.Next;
 		}
@@ -400,7 +265,7 @@ public	class OrderManager
 		return 1;
 	}
 	//OrderManager class public function to view all the orders for a specific food cart
-	public int viewOrders(int foodCartMaker)
+	public int viewOrders(int foodCartMaker, TextWriter writer)
 	{
 		//Nde variable to help us in traversing the order queue
 		LinkedListNode<Order> head = orderQueue.First;
@@ -408,9 +273,9 @@ public	class OrderManager
 		while (head != null) 
 		{
 			//If the particular order is made by the food cart we are interested in
-			if (head.Value.getfoodCartId == foodCartMaker) {
+			if (head.Value.FoodCartId == foodCartMaker) {
 				//Display it
-				head.Value.displayOrder ();
+				head.Value.displayOrder (writer);
 			}
 			//Advance to the next node
 			head = head.Next;
@@ -419,9 +284,9 @@ public	class OrderManager
 		return 1;
 	}
 	///OrderManager class public function to cancel a specified order
-	public int cancelOrder(Order order)
+	public int cancelOrder(Order order, TextWriter writer)
 		{
-		if(order.getStatus()=='s')
+		if(order.OrderStatus=='s')
 			{
 			///Linked list node to help us in searching through the order queue
 			LinkedListNode<Order> head = orderQueue.Find (order);
@@ -429,7 +294,7 @@ public	class OrderManager
 			if (head != null)
 			{
 				///Cancel the order
-				head.Value.setStatus ('c');
+				head.Value.OrderStatus='c';
 				///Remove the order from the order queue
 				orderQueue.Remove (order);
 				///Return successful flag
@@ -445,7 +310,7 @@ public	class OrderManager
 					///Else the order is not in a state where it can be cancelled
 			else
 			{
-			Console.WriteLine ("Order not able to be cancelled");
+			writer.WriteLine ("Order not able to be cancelled");
 			///Return fail flag
 			return 0;
 			}
@@ -491,6 +356,43 @@ public	class OrderManager
 			return 0;
 		}
 		}
+	public Order getOrder(int orderId)
+	{
+		LinkedListNode<Order> head = orderQueue.First;
+
+		bool found = false;
+		while (head != null && found==false) {
+			if (head.Value.getOrderId() == orderId) {
+				found = true;
+				return head.Value;
+			}
+			head = head.Next;
+			}
+
+		Order order=new Order();
+		return order;
+	}
+
+	public LinkedList<Order> getOrders(int foodCartMaker)
+	{
+		LinkedList<Order> foodCartOrders = new LinkedList<Order> ();
+
+		LinkedListNode<Order> head = orderQueue.First;
+
+		if (head == null) {
+			return foodCartOrders;
+		}
+
+		while (head != null) {
+			if (head.Value.getfoodCartId () == foodCartMaker) {
+				foodCartOrders.AddFirst (new Order (head.Value));
+			}
+			head = head.Next;
+		}
+
+		return foodCartOrders;
+	}
+
 	///OrderManager class public function to delay a specified order
 		///Maybe we will whack this functionality....
 	public int delayOrder(Order order)
@@ -501,7 +403,7 @@ public	class OrderManager
 		if (head != null)
 		{
 			///Delay the order
-			head.Value.setStatus ('d');
+			head.Value.OrderStatus.set('d');
 			///Return successful flag
 			return 1;
 		}
